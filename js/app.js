b5e9f3e12f537cc369940ffa9a8dc3757f625895
toadder <zheka20042006@gmail.com>
2022-07-16 22:29:53 +1000

    .

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function setHeight() {
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+setHeight();
+window.addEventListener("resize", setHeight);
